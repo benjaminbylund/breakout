50b1d81428560a710c67b49340ad660da9ce75c5
benjaminbylund <31540367+benjaminbylund@users.noreply.github.com>
2018-12-13 08:27:03 +0100

    Add files via upload
    La och bytte ut konstanter till variabler. Fixade även kollisionen på de ritade bilderna .

--- breakout.js
+++ breakout.js
@@ -1,34 +1,45 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+//bilder
+let paddleImg = document.getElementById("paddleImg");
+let brickImg = document.getElementById("brickImg");
+let ballImg = document.getElementById("ballImg");
+let bgImg = document.getElementById("bgImg");
+
 //ball and canvas variablar
-var ballRadius = 10;
+var ballRadius = 20;
+var ballW = 40;
+var ballH = 40;
+var ballSpeed = 4;
+var ballAcceleration = 0.5;
 var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+var y = canvas.height-ballH;
+var dx = 1;
+var dy = -1;
 
 var score = 0;
 var lives = 3;
 
 //paddle variablar
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width - paddleWidth) / 2;
+var paddleHeight = 20;
+var paddleWidth = 150;
+var paddleX = (canvas.width - paddleWidth) / 2; // 480 - 75 / 2 = 235
 var rightPressed = false;
 var leftPressed = false;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+
 //brick variablar
 var brickRowCount = 3;
 var brickColumnCount = 5;
-var brickWidth = 75;
-var brickHeight = 20;
-var brickPadding = 10;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 30;
+var brickWidth = 150;
+var brickHeight = 40;
+var brickPadding = 20;
+var brickOffsetTop = 60;
+var brickOffsetLeft = 60;
 
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
@@ -44,6 +55,9 @@ function keyDownHandler(e) {
 	}else if(e.keyCode == 37){
 		leftPressed = true;
 	}
+	if(e.keyCode == 32){
+		ballSpeed *= 2;
+ 	}
 }
 
 function keyUpHandler(e){
@@ -56,7 +70,8 @@ function keyUpHandler(e){
 
 function drawPaddle(){
 	ctx.beginPath();
-	ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+	//ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+	ctx.drawImage(paddleImg, paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -65,7 +80,8 @@ function drawPaddle(){
 function drawBall(){
 	ctx.beginPath();
 	 //Arc takes 6 parameters, x, y, arc radius, start angle and end, direction fof drawing.
-	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	//ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	ctx.drawImage(ballImg, x, y, ballW, ballH);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -80,7 +96,8 @@ function drawBricks() {
             bricks[c][r].x = brickX;
             bricks[c][r].y = brickY;
             ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.drawImage(brickImg, brickX, brickY, brickWidth, brickHeight);
+           // ctx.rect(brickX, brickY, brickWidth, brickHeight);
             ctx.fillStyle = "#0095DD";
             ctx.fill();
             ctx.closePath();
@@ -99,6 +116,10 @@ function drawLives() {
 	ctx.font = "16px Arial";
 	ctx.fillStyle = "#0095DD";
 	ctx.fillText("Lives: " + lives, canvas.width-65, 20);
+}
+
+function drawBg() {
+	ctx.drawImage(bgImg,0,0,canvas.width, canvas.height);
 }
 
 function collisionDetection(){
@@ -122,6 +143,7 @@ function collisionDetection(){
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBg();
 	drawBricks();
 	drawBall();
 	drawPaddle();
@@ -130,40 +152,42 @@ function draw() {
 	collisionDetection();
 
 	//om värdet blir lägre än 0 så ändrar den riktning 
-	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+	if(x + (dx * ballSpeed) > canvas.width - ballW || x + (dx * ballSpeed) < 0) {
 		dx = -dx;
 	}
 
 	//Gör så att jag får fram en ruta där det står Game Over
-	if(y + dy < ballRadius) {
+	if(y + (dy * ballSpeed) < 0) {
    		 dy = -dy;
-	} else if(y + dy > canvas.height - ballRadius) {
-  	  if(x > paddleX && x < paddleX + paddleWidth) {
-        dy = -dy;
-    } 
-    else {
-        lives--;
-        if(!lives){
-        	aler("Game Over");
-        	document.location.reload();
-        }
-        else {
-        x = canvas.width/2;
-        y = canvas.height-30;
-        dx = 3;
-        dy = -3;
-        paddleX = (canvas.width-paddleWidth)/2;
-      }
-    }
-}
+	} else if(y + (dy * ballSpeed) > canvas.height - ballH) {
+  	  	if(x > paddleX && x < paddleX + paddleWidth) {
+        	dy = -dy;
+        	ballSpeed += ballAcceleration;
+    	} 
+	    else {
+	        lives--;
+	        if(!lives){
+	        	alert("Game Over");
+	        	document.location.reload();
+	        }
+	        else {
+		        x = canvas.width/2;
+		        y = canvas.height-30;
+		        dx = 1;
+		        dy = -1;
+		        paddleX = (canvas.width-paddleWidth)/2;
+	     	}	
+    	}
+	}
 	if(rightPressed && paddleX < canvas.width - paddleWidth) {
-		paddleX += 7;
+		paddleX += 14;
 	}
 	else if (leftPressed && paddleX > 0){
-		paddleX -= 7;
+		paddleX -= 14;
 	}
-	x += dx;
-	y += dy;
+	
+	x += dx* ballSpeed;
+	y += dy* ballSpeed;
 
 	requestAnimationFrame(draw);
 }
